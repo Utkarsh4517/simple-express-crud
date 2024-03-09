2257f38bc8d6b73c9c157c65cc866d14012ca975
Utkarsh4517 <uttkarsh.dav@gmail.com>
2024-03-10 03:03:22 +0530

    added post method

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import express, { json } from "express";
 import userslist from "./MOCK_DATA.json" assert { type: "json" };
+import fs from "fs";
 const app = express();
+
+// Middleware
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/api/users", (req, res) => res.json(userslist));
 
@@ -20,22 +24,25 @@ app.get("/api/users/:id", (req, res) => {
 });
 
 app.post("/api/users", (req, res) => {
-  // TODO: Create new user
-  return res.json({ status: "pending" });
+  const body = req.body;
+  userslist.push({...body, id: userslist.length +1});
+  fs.writeFile("./MOCK_DATA.json", JSON.stringify(userslist), (err, data) => {
+    return res.json({ status: "success", id: userslist.length +1});
+  });
 });
 
 app.patch("/api/users/:id", (req, res) => {
-    // TODO: Edit an user
+  // TODO: Edit an user
   const id = Number(req.params.id);
   const user = userslist.find((user) => user.id === id);
-  return res.json({ status: "pending"})
+  return res.json({ status: "pending" });
 });
 
 app.delete("/api/users/:id", (req, res) => {
-    // TODO: Delete an user
-    const id = Number(req.params.id);
-    const user = userslist.find((user) => user.id === id);
-    return res.json({ status: "pending"})
-})
+  // TODO: Delete an user
+  const id = Number(req.params.id);
+  const user = userslist.find((user) => user.id === id);
+  return res.json({ status: "pending" });
+});
 
 app.listen(8000, () => "Server started");
